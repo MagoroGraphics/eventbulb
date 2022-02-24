ffc5aabe98c634eec6307aa4c00fa0f02f126214
MagoroGraphics <magoro.graphics@gmail.com>
2022-02-24 20:57:06 +0000

    Added Django Meetup

--- eventbulb/urls.py
+++ eventbulb/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('information.urls')),
+    path('django_meetup', include('information.urls')),
     path('admin/', admin.site.urls),
 ]
